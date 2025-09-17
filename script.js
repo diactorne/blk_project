c7d381e8ef0be293d205345436361332afe4b0dd
unknown <lilianvuillaume@gmail.com>
2025-09-17 20:40:04 +0200

    maj historique et script

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const responses = {
   adieux: ["À bientôt 👋", "Bye bye !", "Ciao !", "Au revoir !"],
   commandes: ["Voici la liste des commandes : /help, /commande(s), /aide, /contacts"],
   inconnu: ["Je ne suis pas sûr de comprendre…", "Hmm, je n’ai pas encore appris ça.", "Essaie autre chose !"],
-  match: ["Prochainement"],
+  match: ["#Rio Ave-FC Porto(Portugal) 19/09/25: V2 @1.41."],
   combines: ["Prochainement"]
 };
 
